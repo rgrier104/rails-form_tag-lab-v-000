eaee4a842010526a235f82969ed925de5ae96267
Rachel Grier <rgrier104@gmail.com>
2019-09-11 21:15:31 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def allowed_params
     params.require(:student).permit(:first_name, :last_name)
-  end  
+  end
 
   def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
